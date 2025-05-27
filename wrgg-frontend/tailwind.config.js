a27cfdbbcd5aadd007b5ed7225bcbcdde892f40a
taiking0905 <taiki.noguchi0905@gmail.com>
2025-05-27 15:39:28 +0900

    Taileindもできた

--- wrgg-frontend/tailwind.config.js
+++ wrgg-frontend/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
