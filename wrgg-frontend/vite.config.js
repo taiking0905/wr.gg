fe09ef9f908da514a0d51344fcc99c5beebe75a0
taiking0905 <taiki.noguchi0905@gmail.com>
2025-05-30 15:40:44 +0900

    test

--- wrgg-frontend/vite.config.js
+++ wrgg-frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/wr.gg/',
   build: {
-    outDir: 'docs',
+    outDir: '../docs',
   },
   plugins: [react()],
 })
